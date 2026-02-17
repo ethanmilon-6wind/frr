ethanmilon-6wind/frr

commit 73f72c52668cb402913ac694d91c25ee48cb174a

lib: add ability to log from external pthread
External libraries can re-enter the FRR code through a hook function. A
crash occurs when logging from this hook function if the library has
initiated a new pthread, as the FRR RCU context is not initialized for
this thread.

Add frr_pthread_non_controlled_startup() function to initialize a valid
RCU context within a FRR pthread context, originating from an external
pthread.

Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

lib/frr_pthread.c
@@ -220,6 +220,37 @@ void frr_pthread_stop_all(void)
 	}
 }
 
+static void *frr_pthread_attr_non_controlled_start(void *arg)
+{
+	struct frr_pthread *fpt = arg;
+
+	fpt->running = true;
+
+	return NULL;
+}
+
+/* Create a FRR pthread context from a non FRR pthread initialized from an
+ * external library in order to allow logging */
+int frr_pthread_non_controlled_startup(pthread_t thread, const char *name,
+				       const char *os_name)
+{
+	struct frr_pthread_attr attr = {
+		.start = frr_pthread_attr_non_controlled_start,
+		.stop = frr_pthread_attr_default.stop,
+	};
+	struct frr_pthread *fpt;
+
+	fpt = frr_pthread_new(&attr, name, os_name);
+	if (!fpt)
+		return -1;
+
+	fpt->thread = thread;
+	fpt->rcu_thread = rcu_thread_new(NULL);
+	frr_pthread_inner(fpt);
+
+	return 0;
+}
+
 /*
  * ----------------------------------------------------------------------------
  * Default Event Loop

lib/frr_pthread.h
@@ -202,6 +202,9 @@ void frr_pthread_stop_all(void);
 #define pthread_condattr_setclock(A, B)
 #endif
 
+int frr_pthread_non_controlled_startup(pthread_t thread, const char *name,
+				       const char *os_name);
+
 /* mutex auto-lock/unlock */
 
 /* variant 1:

lib/frrcu.c
@@ -149,20 +149,9 @@ static struct rcu_thread *rcu_self(void)
 	return (struct rcu_thread *)pthread_getspecific(rcu_thread_key);
 }
 
-/*
- * thread management (for the non-main thread)
- */
-struct rcu_thread *rcu_thread_prepare(void)
+struct rcu_thread *rcu_thread_new(void *arg)
 {
-	struct rcu_thread *rt, *cur;
-
-	rcu_assert_read_locked();
-
-	if (!rcu_active)
-		rcu_start();
-
-	cur = rcu_self();
-	assert(cur->depth);
+	struct rcu_thread *rt, *cur = arg;
 
 	/* new thread always starts with rcu_read_lock held at depth 1, and
 	 * holding the same epoch as the parent (this makes it possible to
@@ -172,11 +161,30 @@ struct rcu_thread *rcu_thread_prepare(void)
 	rt->depth = 1;
 
 	seqlock_init(&rt->rcu);
-	seqlock_acquire(&rt->rcu, &cur->rcu);
+	if (cur)
+		seqlock_acquire(&rt->rcu, &cur->rcu);
 
 	rcu_threads_add_tail(&rcu_threads, rt);
 
 	return rt;
+}
+
+/*
+ * thread management (for the non-main thread)
+ */
+struct rcu_thread *rcu_thread_prepare(void)
+{
+	struct rcu_thread *cur;
+
+	rcu_assert_read_locked();
+
+	if (!rcu_active)
+		rcu_start();
+
+	cur = rcu_self();
+	assert(cur->depth);
+
+	return rcu_thread_new(cur);
 }
 
 void rcu_thread_start(struct rcu_thread *rt)

lib/frrcu.h
@@ -40,6 +40,12 @@ extern "C" {
 /* opaque */
 struct rcu_thread;
 
+/* sets up rcu thread info
+ *
+ * return value must be passed into the thread's call to rcu_thread_start()
+ */
+extern struct rcu_thread *rcu_thread_new(void *arg);
+
 /* called before new thread creation, sets up rcu thread info for new thread
  * before it actually exits.  This ensures possible RCU references are held
  * for thread startup.

commit b32984956ad2744d3c173424edaae572cf24efd4

lib: fix shutdown crash due to thread ID recycling
External threads (such as those from rtrlib) that enter FRR code for
logging require RCU data to be initialized. Previously, this was handled
by frr_pthread_non_controlled_startup(), which registered these external
threads as full FRR pthreads.

However, rtrlib can restart its threads (e.g., during a config reset).
Since FRR is not notified of these terminations, the old thread IDs
remain in the managed list. If the OS recycles a thread ID, FRR attempts
to register it again, leading to duplicates. This causes a crash during
shutdown when FRR tries to join the same thread ID twice.

Initialize RCU state for the calling thread without adding it to the
global FRR pthread list.

Backtrace:
  #1  __pthread_kill_internal (signo=11, threadid=<optimized out>)
  #2  __GI___pthread_kill (threadid=<optimized out>,
      signo=signo@entry=11)
  #3  __GI_raise (sig=11)
  #4  core_handler (signo=11, siginfo=0x7ffcf1c967f0,
      context=0x7ffcf1c966c0)
  #5  <signal handler called>
  #6  __pthread_clockjoin_ex (threadid=130041090406080,
      thread_return=0x0, clockid=0, abstime=0x0, block=true)
  #7  fpt_halt (fpt=0x764578001b60, res=0x0)
  #8  frr_pthread_stop (fpt=0x764578001b60, result=0x0)
  #9  frr_pthread_stop_all ()
  #10 bgp_pthreads_finish ()
  #11 bgp_exit (status=0)
  #12 sigint ()
  #13 frr_sigevent_process ()
  #14 event_fetch (m=0x5e83ecaaae90, fetch=0x7ffcf1c96eb0)
  #15 frr_run (master=0x5e83ecaaae90)
  #16 main (argc=9, argv=0x7ffcf1c97138)

Fixes: 73f72c5 ("lib: add ability to log from external pthread")
Fixes: fdaf08b ("bgpd: fix logging from rpki_create_socket()")
Signed-off-by: Ethan Milon <ethan.milon@6wind.com>

lib/frr_pthread.c
@@ -253,15 +253,6 @@ void frr_pthread_stop_all(void)
 	}
 }
 
-static void *frr_pthread_attr_non_controlled_start(void *arg)
-{
-	struct frr_pthread *fpt = arg;
-
-	fpt->running = true;
-
-	return NULL;
-}
-
 /* Create a FRR pthread context from a non FRR pthread initialized from an
  * external library in order to allow logging */
 int frr_pthread_non_controlled_startup(pthread_t thread, const char *name,
@@ -270,22 +261,6 @@ int frr_pthread_non_controlled_startup(pthread_t thread, const char *name,
 	struct rcu_thread *rcu_thread = rcu_thread_new(NULL);
 
 	rcu_thread_start(rcu_thread);
-
-	struct frr_pthread_attr attr = {
-		.start = frr_pthread_attr_non_controlled_start,
-		.stop = frr_pthread_attr_default.stop,
-	};
-	struct frr_pthread *fpt;
-
-	fpt = frr_pthread_new(&attr, name, os_name);
-	if (!fpt)
-		return -1;
-
-	fpt->thread = thread;
-	fpt->rcu_thread = rcu_thread;
-	fpt->started = true;
-
-	frr_pthread_inner(fpt);
 
 	return 0;
 }